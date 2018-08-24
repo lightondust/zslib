456955b8954943c62c402fba4e0d44368e4ce048
Osamu Ueno <osamu.ueno@hmcom.co.jp>
2018-08-24 21:10:01 +0900

    chainer_util修正、report項目カスタマイズ

--- src/chainer_util.py
+++ src/chainer_util.py
@@ -1,28 +1,31 @@
 import chainer
-import chainer.functions as F
-import chainer.links as L
 from chainer import training
 from chainer.training import extensions
 
 
-def get_iter(train, test, batchsize=300):
+BATCHSIZE = 300
+EPOCH = 500
+
+
+def get_iter(train, test, batchsize=BATCHSIZE):
     train_iter = chainer.iterators.SerialIterator(train, batch_size=batchsize)
     test_iter = chainer.iterators.SerialIterator(test, batch_size=batchsize,
                                                  repeat=False, shuffle=False)
     return train_iter, test_iter
 
 
-def save(model_path, model):
+def save_model(model_path, model):
     print('save model to:{}'.format(model_path))
     model.to_cpu()
     chainer.serializers.save_hdf5(model_path, model, compression=4)
 
 
-def load(model_path, model):
+def load_model(model_path, model):
     chainer.serializers.load_hdf5(model_path, model)
 
 
-def get_trainer(model, train_iter, test_iter, loss_func=None, epoch=500):
+def get_trainer(model, train_iter, test_iter, loss_func=None, epoch=EPOCH,
+                report='', progress_bar=False):
     # Setup an optimizer
     optimizer = chainer.optimizers.Adam()
     optimizer.setup(model)
@@ -38,7 +41,13 @@ def get_trainer(model, train_iter, test_iter, loss_func=None, epoch=500):
                                         eval_func=loss_func))
     trainer.extend(extensions.dump_graph('main/loss'))
     trainer.extend(extensions.LogReport())
-    trainer.extend(extensions.PrintReport(
-        ['epoch', 'main/loss', 'validation/main/loss', 'elapsed_time']))
-    # trainer.extend(extensions.ProgressBar())
+
+    if report == '':
+        report = ['epoch', 'main/loss', 'validation/main/loss', 'elapsed_time']
+
+    trainer.extend(extensions.PrintReport(report))
+
+    if progress_bar:
+        trainer.extend(extensions.ProgressBar())
+
     return trainer
