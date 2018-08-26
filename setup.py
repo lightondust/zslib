0dbc9c6d1c88f3d0fe8756c615685f584481840f
zs mcp <choumori@choumorinoMacBook-puro.local>
2018-08-26 11:36:52 +0900

    pip インストール用

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="zslib",
+      version="0.1",
+      description="",
+      url="https://github.com/lightondust/zslib.git",
+      packages=find_packages()
+      )
