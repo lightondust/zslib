286b5cd89f56f255f6298377d49e7fd0d95bb77c
zs <lightondust@gmail.com>
2019-07-23 16:00:56 +0900

    file load save

--- zslib/util/file_util.py
+++ zslib/util/file_util.py
@@ -1,10 +1,28 @@
 import sys, os
+import json
+import pickle
 
 
 def make_dir(dir_path):
     if not os.path.exists(dir_path):
         os.mkdir(dir_path)
         os.chmod(dir_path, mode=0o775)
+
+
+def save_to_json(dict_obj, path):
+    with open(path, 'w') as f:
+        json.dump(dict_obj, f)
+
+
+def save(path, obj):
+    with open(path, 'wb') as f:
+        pickle.dump(obj, f)
+
+
+def load(path):
+    with open(path, 'rb') as f:
+        obj = pickle.load(f)
+    return obj
 
 
 # package
