f11499b6cbe8d424ef3f8469326240b3e85c36ec
zs <lightondust@gmail.com>
2019-08-13 23:34:38 +0900

    time string for log file name

--- zslib/util/datetime_util.py
+++ zslib/util/datetime_util.py
@@ -5,5 +5,20 @@ def get_today():
     return datetime.now().date()
 
 
+def get_time_for_file_name(precision='minute'):
+    """
+    :param precision: 'm', 'h', 's', 'd'
+    :return:
+    """
+    if precision in ['minute', 'm']:
+        return datetime.strftime(datetime.now(), '%Y%m%d%H%M')
+    elif precision in ['hour', 'h']:
+        return datetime.strftime(datetime.now(), '%Y%m%d%H')
+    elif precision in ['second', 's']:
+        return datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
+    elif precision in ['day', 'd']:
+        return datetime.strftime(datetime.now(), '%Y%m%d')
+
+
 def jp_string_to_datetime(jp_str, date_format='%Y年%m月%d日'):
     return datetime.strptime(jp_str, date_format)
