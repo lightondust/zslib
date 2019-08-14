a9f6d2a7c53f9d140541806c8009955aa5f765f2
zs <lightondust@gmail.com>
2019-08-15 01:23:47 +0900

    read csv list

--- zslib/util/pandas_util.py
+++ zslib/util/pandas_util.py
@@ -1,4 +1,5 @@
 from ast import literal_eval
+import pandas as pd
 
 
 def string_to_list_data(data):
@@ -7,3 +8,10 @@ def string_to_list_data(data):
 
 def string_to_list_column(data_series):
     return data_series.apply(string_to_list_data())
+
+
+def csv_list_to_dataframe(file_list):
+    review_df = pd.DataFrame()
+    for file in file_list:
+        review_df = review_df.append(pd.read_csv(file, index_col=0), ignore_index=True)
+    return review_df
