1d0626636646b9916bf5bcd7fe96a830ce82e633
zs <lightondust@gmail.com>
2019-08-31 23:21:06 +0900

    elenium module, header transformer

--- zslib/util/scraping_util.py
+++ zslib/util/scraping_util.py
@@ -1,9 +1,11 @@
 import requests
 from requests.compat import urljoin
+from selenium import webdriver
 from lxml import html
 import os, time
 
 TIME_SLEEP = 1
+driver = webdriver.Chrome
 
 
 def get_html_obj(url, headers={}, sleep=TIME_SLEEP, encoding='auto'):
@@ -15,6 +17,34 @@ def get_html_obj(url, headers={}, sleep=TIME_SLEEP, encoding='auto'):
     html_obj = html.fromstring(response.text)
     time.sleep(sleep)
     return html_obj
+
+
+def get_html_obj_from_selenium(url, driver, sleep=TIME_SLEEP):
+    """
+    :param url:
+    :param sleep:
+    :param driver:
+    :return:
+
+    from scraping_util import driver
+    dr = driver()
+    html_obj = get_html_obj_from_selenium(url, 1, dr)
+    dr.close()
+    """
+    driver.get(url)
+    time.sleep(sleep)
+    html_obj = html.fromstring(driver.page_source)
+    return html_obj
+
+
+def header_from_raw_string(head_str):
+    head_lst = [l.split(':') for l in head_str.split('\n')]
+
+    headers = {}
+    for h in head_lst:
+        headers[h[0]] = ''.join(h[1:]).strip()
+
+    return headers
 
 
 def element_to_html_string(element):
