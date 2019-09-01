dce4fabed4014d686deb240d0ac3e7bc12962c51
zs <lightondust@gmail.com>
2019-09-01 22:56:21 +0900

    convert string to list, dataframe to list of dict

--- zslib/util/pandas_util.py
+++ zslib/util/pandas_util.py
@@ -6,8 +6,20 @@ def string_to_list_data(data):
     return literal_eval(data)
 
 
-def string_to_list_column(data_series):
-    return data_series.apply(string_to_list_data())
+def string_to_list_series(data_series):
+    return data_series.apply(string_to_list_data)
+
+
+def string_to_list_dataframe(data_frame, column_name):
+    data_frame[column_name] = string_to_list_series(data_frame[column_name])
+    return data_frame
+
+
+def dataframe_to_list_of_dict(data_frame):
+    list_of_dict = []
+    for idx, item in data_frame.iterrows():
+        list_of_dict.append(item.to_dict())
+    return list_of_dict
 
 
 def csv_list_to_dataframe(file_list):
